8ee65e288fe3e51b043295eb0b8457cc5535816d
Abdiel Moran <abdiel.moran@upr.edu>
2016-10-16 03:42:22 -0600

    yasssss

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -7,10 +7,10 @@ export class InMemoryDataService implements InMemoryDbService {
 
         let shouts = [
 
-            {id:1, rating:6, userID:1, commentSectionId:1, title:"Primera Prueba", message:"Todos aqui somos unos motherfucking wierdos."},
-            {id:2, rating:9, userID:2, commentSectionId:2, title:"Segunda Prueba", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
-            {id:3, rating:0, userID:3, commentSectionId:3, title:"Tercer Prueba", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
-            {id:4, rating:1, userID:4, commentSectionId:4, title:"Cuarta Prueba", message:"adfasdfadsfasdfadsfadfasfasdfadf"}
+            {id:1, rating:6, userID:1, commentSectionId:1, title:"Quejas de Ale", message:"Todos aqui somos unos motherfucking wierdos."},
+            {id:2, rating:9, userID:2, commentSectionId:2, title:"Ale es un mamabicho", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
+            {id:3, rating:0, userID:3, commentSectionId:3, title:"Ale se la traga", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
+            {id:4, rating:1, userID:4, commentSectionId:4, title:"Abdiel es jesu", message:"adfasdfadsfasdfadsfadfasfasdfadf"}
 
         ]; 
 
