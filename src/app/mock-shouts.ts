f1d27380b6d100b96d4619ccfe790ad5484ec8a7
Abdiel Moran <abdiel.moran@upr.edu>
2016-10-16 03:14:11 -0600

    Cambio en los mocks, CSS, HTML

--- src/app/mock-shouts.ts
+++ src/app/mock-shouts.ts
@@ -2,9 +2,9 @@ import {Shouts} from './shouts'
 
 export const SHOUTS: Shouts[] = [
 
-    {id:1, rating:0, userID:1, commentSectionId:1, title:"Primera Prueba", message:"Todos aqui somos unos motherfucking wierdos." },
-    {id:2, rating:0, userID:2, commentSectionId:2, title:"Segunda Prueba", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
+    {id:1, rating:8, userID:1, commentSectionId:1, title:"Primera Prueba", message:"Todos aqui somos unos motherfucking wierdos." },
+    {id:2, rating:4, userID:2, commentSectionId:2, title:"Segunda Prueba", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
     {id:3, rating:0, userID:3, commentSectionId:3, title:"Tercera Prueba", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
-    {id:4, rating:0, userID:4, commentSectionId:4, title:"Cuarta Prueba", message:"adfasdfadsfasdfadsfadfasfasdfadf"}
+    {id:4, rating:6, userID:4, commentSectionId:4, title:"Cuarta Prueba", message:"adfasdfadsfasdfadsfadfasfasdfadf"}
 
 ]
