35e155b579339855892fb74fd0a519c9367a959b
Abdiel Moran <abdiel.moran@upr.edu>
2016-10-16 01:23:55 -0600

    Cambios a los mock shouts

--- src/app/mock-shouts.ts
+++ src/app/mock-shouts.ts
@@ -3,8 +3,8 @@ import {Shouts} from './shouts'
 export const SHOUTS: Shouts[] = [
 
     {id:1, rating:0, userID:1, commentSectionId:1, title:"Primera Prueba", message:"Todos aqui somos unos motherfucking wierdos." },
-    {id:2, rating:0, userID:1, commentSectionId:2, title:"LOREM IPSUM", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
-    {id:3, rating:0, userID:1, commentSectionId:3, title:"LOREM IPSUM", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
-    {id:4, rating:0, userID:1, commentSectionId:4, title:"LOREM IPSUM", message:"adfasdfadsfasdfadsfadfasfasdfadf"}
+    {id:2, rating:0, userID:2, commentSectionId:2, title:"Segunda Prueba", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
+    {id:3, rating:0, userID:3, commentSectionId:3, title:"Tercera Prueba", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
+    {id:4, rating:0, userID:4, commentSectionId:4, title:"Cuarta Prueba", message:"adfasdfadsfasdfadsfadfasfasdfadf"}
 
 ]
