75e0ba4f2e8f1c043fce398aa81a7e4b0baefa4a
Alexander Rivera <arivepr@outlook.com>
2016-11-30 22:35:49 -0400

    Bregando con usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
@@ -38,11 +37,8 @@ import { LoginComponent } from './login/login.component';
         CreateShoutComponent,
         ShoutsComponent,
         ShoutDetailsComponent,
-        AnalyticsComponent
-
-,
-    Login.component.tsComponent,
-    LoginComponent
+        AnalyticsComponent,
+        LoginComponent
 ],
     imports: [
         BrowserModule,
@@ -62,65 +58,5 @@ import { LoginComponent } from './login/login.component';
 
 export class AppModule {
 }
-=======
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {HttpModule, JsonpModule} from '@angular/http';
-
-import {AppComponent} from './app.component';
-import {routing, appRoutingProviders} from './app.routing';
-import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {ShoutsComponent} from './shouts/shouts.component';
-import {CommentsComponent} from './comment-section/comment.component';
-import {CreateShoutComponent} from './create-shout/create-shout.component';
-import {ShoutDetailsComponent} from './shout-details/shout-details.component';
-import {AnalyticsComponent} from './analytics/analytics.component';
-import {Departamento} from './deps';
 
 
-// Services
-import {ShoutsService} from './services/shouts.service'
-import {CommentsService} from './services/comments.service';
-
-// In memory web api, para simular http
-import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
-import {InMemoryDataService} from './services/in-memory-data.service';
-
-
-
-@NgModule({
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        AboutComponent,
-        DashboardComponent,
-        CommentsComponent,
-        CreateShoutComponent,
-        ShoutsComponent,
-        ShoutDetailsComponent,
-        AnalyticsComponent
-
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpModule,
-        JsonpModule,
-        routing,
-        InMemoryWebApiModule.forRoot(InMemoryDataService)
-    ],
-    providers: [
-        appRoutingProviders,
-        CommentsService,
-        ShoutsService,
-        Departamento
-        ],
-    bootstrap: [AppComponent]
-})
-
-export class AppModule {
-}
->>>>>>> a4031fafcdcffb84735a1775c5519e2ef95ebd87
--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -7,7 +7,7 @@ export class InMemoryDataService implements InMemoryDbService {
 
         let shouts = [
 
-            {id:1, rating:6, userID:"Alexander Rivera", commentSectionId:1, title:"Cobros tardios departe de @Recursos Humanos", 
+            {id:1, rating:6, userID:"Anon2", commentSectionId:1, title:"Cobros tardios departe de @Recursos Humanos", 
             message:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
             {id:2, rating:9, userID:"Abdiel Moran", commentSectionId:2, title:"Falta de mantenimiento en laboratorio ac-232", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
             {id:3, rating:0, userID:"Andre Walker", commentSectionId:3, title:"Humedad en conductos de aires", message:"adfasdfadsfasdfadsfadfasfasdfadf"},
