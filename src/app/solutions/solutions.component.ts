bb7699d11fbfcfae9e7b7314ddfdc1424b1d2d51
Alexander Rivera <arivepr@outlook.com>
2016-12-02 16:05:29 -0400

    Implementando analytics

---
+++ src/app/solutions/solutions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solutions',
+  template: require('./solutions.component.html'),
+  styles: [require('./solutions.component.css')]
+})
+export class SolutionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
