433763d3f87202a631a1a67db42ca876e54eea70
robertjdev <r.j.r.eol@gmail.com>
2016-12-02 17:37:26 -0400

    Added User Endpoint URL (Not Tested)

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,20 +1,42 @@
 import { Injectable } from '@angular/core';
-import {Headers, Http} from '@angular/http';
 import {Users} from '../users';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 
 @Injectable()
 export class UsersService {
 
-    private usersURL ='app/users';
-    private headers  = new Headers({"Content-Type" : "application.json"});
+    private usersURL ='http://api.neighbornet.io/api/users';
+    private headers = new Headers({ 'Content-Type': 'application/json', 'charset': 'UTF-8' });
+    private options = new RequestOptions({ headers: this.headers });
 
-    constructor(
-        private http: Http
-    ) { }
+    constructor(private http: Http) { }
 
+    getUsers() : Observable<[]> {
 
-    
+        // ...using get request
+        return this.http.get(this.usersURL)
+                       // ...and calling .json() on the response to return data
+                        .map((res:Response) => res.json())
+                        //...errors if any
+                        .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
 
+    }
+
+    getCats() {
+      return this.http.get(this.usersURL).map(res => res.json());
+    }
+
+    addUser(usr) {
+      return this.http.post(this.usersURL, JSON.stringify(usr), this.options);
+    }
+
+    editUser(usr) {
+      return this.http.put(this.usersURL`/${usr._id}`, JSON.stringify(usr), this.options);
+    }
+
+    deleteCat(usr) {
+      return this.http.delete(this.usersURL`/${usr._id}`, this.options);
+    }
 }
-
