a05350897ad59c2c37d0a625fb794385c13a8a3e
robertjdev <r.j.r.eol@gmail.com>
2016-12-08 07:42:10 -0400

    Added Solution Component

--- src/app/services/comments.service.ts
+++ src/app/services/comments.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Comments } from '../comments';
-import { Headers, Http, Response} from '@angular/http';
+import { Headers, Http, Response, RequestOptions} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 
 
@@ -9,14 +9,21 @@ export class CommentsService {
 
     private commentsURL = 'http://api.neighbornet.io/comment';
     private commentShoutURL = 'http://api.neighbornet.io/comment/shout';
-    private headers = new Headers({"Content-Type": "application/json"});
+    private headers = new Headers({ 'Content-Type': 'application/json' });
+    private options = new RequestOptions({ headers: this.headers });
 
     constructor(
         private http: Http
     ) { }
 
+    getComment(id: string): Observable<Comments>{
+      const url = `${this.commentsURL}/${id}`;
+      return this.http.get(url)
+                      .map((res:Response) => res.json())
+                      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
     getComments(shoutId: string): Observable<Comments[]>{
-      console.log('miramos el id:' + shoutId);
       const url = `${this.commentShoutURL}/${shoutId}`;
       return this.http.get(url)
                       .map((res:Response) => res.json())
@@ -28,5 +35,11 @@ export class CommentsService {
         return this.http.post(this.commentsURL,JSON.stringify(comment),{headers: this.headers})
                         .map((res:Response) => res.json())
                         .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    updateComment(comment: Comments): Observable<Comments> {
+      return this.http.put(`${this.commentsURL}/${comment.id}`,JSON.stringify(comment), this.options)
+                      .map((res:Response) => res.json())
+                      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
     }
 }
