915cdadbd5c7f214bde9c514d65383d990d05241
RoyMamba0329 <roy.mamba0329@gmail.com>
2016-12-08 00:12:04 -0400

    vhk

--- src/app/shout-details/shout-details.component.ts
+++ src/app/shout-details/shout-details.component.ts
@@ -15,13 +15,11 @@ export class ShoutDetailsComponent implements OnInit {
 
     // @Input() shout = new Shouts();
     shout:Shouts;
-<<<<<<< HEAD
+
     temp: Shouts[];
     commentSection: Comment[];
-=======
-    temp: Shouts;
->>>>>>> 43e2a31b3dd6f1743e616b794b5fd5a87e684c23
 
+    
     constructor(
         private commentService: CommentsService,
         private shoutService: ShoutsService,
